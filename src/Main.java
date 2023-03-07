395d95da6caacd5d485226839bacc1c67fc41e8f
”Camopogok” <zagrebin.dm@gmail.com>
2023-03-07 18:27:36 +0300

    Задание 3_10_4

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,54 @@ import java.sql.Statement;
 
 public class Main {
 
-        // Блок объявления констант
-        public static final String DB_URL = "jdbc:sqlite:/d:/dev/StepikLesson_3_10/db/My_cats.db";
-        public static final String DB_Driver = "org.sqlite.JDBC";
+    // Блок объявления констант
+    public static final String DB_URL = "jdbc:sqlite:/d:/dev/StepikLesson_3_10/db/My_cats.db";
+    public static final String DB_Driver = "org.sqlite.JDBC";
+    public static Connection connection;
+    public static Statement statement;
 
-        public static void main(String[] args) {
-            try {
-                Class.forName(DB_Driver); //Проверяем наличие JDBC драйвера для работы с БД
-                Connection connection = DriverManager.getConnection(DB_URL);//создание БД
-                System.out.println("Соединение с СУБД выполнено.");
+    public static void main(String[] args) {
+        connectDB();
+        insert_type("Абиссинская кошка");
+        insert_type("Австралийский мист");
+        insert_type("Американская жесткошерстная");
+        disconnectDB();
+    }
 
-                Statement statement = connection.createStatement();
-                statement.execute("CREATE TABLE if not exists 'types' ('id' INTEGER PRIMARY KEY UNIQUE, 'type' varchar(100) NOT NULL)");
-
-                connection.close();       // отключение от БД
-                System.out.println("Отключение от СУБД выполнено.");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace(); // обработка ошибки  Class.forName
-                System.out.println("JDBC драйвер для СУБД не найден!");
-            } catch (SQLException e) {
-                e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
-                System.out.println("Ошибка SQL !");
-            }
+    public static void insert_type(String type) {
+        try {
+            System.out.println("INSERT into types (type) values ('" + type + "')");
+            statement.execute("INSERT into types (type) values ('" + type + "')");
+        } catch (SQLException e) {
+            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
+            System.out.println("Ошибка SQL !");
         }
     }
+
+    public static void connectDB() {
+        try {
+            Class.forName(DB_Driver); //Проверяем наличие JDBC драйвера для работы с БД
+            connection = DriverManager.getConnection(DB_URL);//создание БД
+            System.out.println("Соединение с СУБД выполнено.");
+            statement = connection.createStatement();
+            System.out.println("Готов к выполнению SQL-запросов");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(); // обработка ошибки  Class.forName
+            System.out.println("JDBC драйвер для СУБД не найден!");
+        } catch (SQLException e) {
+            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
+            System.out.println("Ошибка SQL !");
+        }
+    }
+
+    public static void disconnectDB() {
+        try {
+            connection.close();
+            System.out.println("Отключение от СУБД выполнено.");
+        } catch (SQLException e) {
+            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
+            System.out.println("Ошибка SQL !");
+        }
+    }
+}
 
