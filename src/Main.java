3ba37b5b5d34385e9229423a518835163c231136
”Camopogok” <zagrebin.dm@gmail.com>
2023-03-07 18:43:05 +0300

    Задание 3_10_5

--- src/Main.java
+++ src/Main.java
@@ -10,29 +10,89 @@ public class Main {
     public static final String DB_Driver = "org.sqlite.JDBC";
     public static Connection connection;
     public static Statement statement;
+    static String[] types = new String[]{"Абиссинская кошка",
+            "Австралийский мист",
+            "Американская жесткошерстная",
+            "Американская короткошерстная",
+            "Американский бобтейл",
+            "Американский кёрл",
+            "Балинезийская кошка",
+            "Бенгальская кошка",
+            "Бирманская кошка",
+            "Бомбейская кошка",
+            "Бразильская короткошёрстная",
+            "Британская длинношерстная",
+            "Британская короткошерстная",
+            "Бурманская кошка",
+            "Бурмилла кошка",
+            "Гавана",
+            "Гималайская кошка",
+            "Девон-рекс",
+            "Донской сфинкс",
+            "Европейская короткошерстная",
+            "Египетская мау",
+            "Канадский сфинкс",
+            "Кимрик",
+            "Корат",
+            "Корниш-рекс",
+            "Курильский бобтейл",
+            "Лаперм",
+            "Манчкин",
+            "Мейн-ку́н",
+            "Меконгский бобтейл",
+            "Мэнкс кошка",
+            "Наполеон",
+            "Немецкий рекс",
+            "Нибелунг",
+            "Норвежская лесная кошка",
+            "Ориентальная кошка",
+            "Оцикет",
+            "Персидская кошка",
+            "Петерболд",
+            "Пиксибоб",
+            "Рагамаффин",
+            "Русская голубая кошка",
+            "Рэгдолл",
+            "Саванна",
+            "Селкирк-рекс",
+            "Сиамская кошка",
+            "Сибирская кошка",
+            "Сингапурская кошка",
+            "Скоттиш-фолд",
+            "Сноу-шу",
+            "Сомалийская кошка",
+            "Тайская кошка",
+            "Тойгер",
+            "Тонкинская кошка",
+            "Турецкая ангорская кошка",
+            "Турецкий ван",
+            "Украинский левкой",
+            "Чаузи",
+            "Шартрез",
+            "Экзотическая короткошерстная",
+            "Японский бобтейл"
+    };
 
     public static void main(String[] args) {
         connectDB();
-        insert_type("Абиссинская кошка");
-        insert_type("Австралийский мист");
-        insert_type("Американская жесткошерстная");
+        add_all_types();
         disconnectDB();
     }
 
     public static void insert_type(String type) {
         try {
-            System.out.println("INSERT into types (type) values ('" + type + "')");
+            System.out.println("Добавлен тип кошки: " + type);
             statement.execute("INSERT into types (type) values ('" + type + "')");
         } catch (SQLException e) {
-            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
-            System.out.println("Ошибка SQL !");
+            e.printStackTrace(); // обработка ошибок SQL
+            System.out.println("Ошибка SQL!");
         }
     }
 
     public static void connectDB() {
         try {
             Class.forName(DB_Driver); //Проверяем наличие JDBC драйвера для работы с БД
-            connection = DriverManager.getConnection(DB_URL);//создание БД
+            connection = DriverManager.getConnection(DB_URL); //соединение с БД
             System.out.println("Соединение с СУБД выполнено.");
             statement = connection.createStatement();
             System.out.println("Готов к выполнению SQL-запросов");
@@ -40,8 +100,8 @@ public class Main {
             e.printStackTrace(); // обработка ошибки  Class.forName
             System.out.println("JDBC драйвер для СУБД не найден!");
         } catch (SQLException e) {
-            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
-            System.out.println("Ошибка SQL !");
+            e.printStackTrace(); // обработка ошибок SQL
+            System.out.println("Ошибка SQL!");
         }
     }
 
@@ -50,9 +110,16 @@ public class Main {
             connection.close();
             System.out.println("Отключение от СУБД выполнено.");
         } catch (SQLException e) {
-            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
-            System.out.println("Ошибка SQL !");
+            e.printStackTrace(); // обработка ошибок SQL
+            System.out.println("Ошибка SQL!");
         }
+    }
+
+    public static void add_all_types()  {
+        for (int i = 0; i < types.length; i++) {
+            insert_type(types[i]);
+        }
+
     }
 }
 
